36e8503bc3a36a1642ec6e7c46cd7f467b80beed
David Ma <davidma415@gmail.com>
2018-09-08 22:14:44 -0400

    Finished assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
+var light = document.querySelector('.switch.on');
+var background = document.querySelector('.light');
+var message = document.querySelector('.status');
 
+light.addEventListener('click', function() {
+  light.classList.toggle('off');
+  background.classList.toggle('dark');
 
+  if (light.classList.contains('off')) {
+    message.innerHTML = 'Hey, who turned the lights off?'
+  } else {
+    message.innerHTML = "It's too bright in here!";
+  }
+})
